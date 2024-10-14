6273f8030fe3abe05afc5750a51a2cc00c7512b9
ngari-kev <kevngariwangui@gmail.com>
2024-10-14 15:37:51 +0300

    linting

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,16 +1,16 @@
 export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
-    // replace the function below
-    // const self = this;
-    // this.addNeighborhood = function add(newNeighborhood) {
-    //   self.sanFranciscoNeighborhoods.push(newNeighborhood);
-    //   return self.sanFranciscoNeighborhoods;
-    //};
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-    const self = this;
-    this.addNeighborhood = (newNeighborhood) => {
-        self.sanFranciscoNeighborhoods.push(newNeighborhood);
-        return self.sanFranciscoNeighborhoods;
-    };
-  }
+  // replace the function below
+  // const self = this;
+  // this.addNeighborhood = function add(newNeighborhood) {
+  //   self.sanFranciscoNeighborhoods.push(newNeighborhood);
+  //   return self.sanFranciscoNeighborhoods;
+  // };
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
