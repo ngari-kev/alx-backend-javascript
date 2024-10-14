3aea858ad4c7d383a4992499af29286dda777ed6
ngari-kev <kevngariwangui@gmail.com>
2024-10-14 17:57:50 +0300

    loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -9,10 +9,10 @@
 export default function appendToEachArrayValue(array, appendString) {
   let index = 0;
   const array2 = array;
-  for (const value of array2) {
+  for (const value of array) {
     array2[index] = appendString + value;
     index += 1;
   }
 
-  return array;
+  return array2;
 }
