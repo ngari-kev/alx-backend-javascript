9678030013ca0c6bc245cf1b2a21f8d871df51bf
ngari-kev <kevngariwangui@gmail.com>
2024-11-25 20:23:31 +0300

    inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+    constructor(sqft, floors) {
+        super(sqft);
+        this._floors = floors;
+    }
+
+    get floors() {
+        return this._floors;
+    }
+
+    set floors(floorCount) {
+        if (!(floorCount <= 0 || typeof floorCount === 'number')) {
+            throw new TypeError('floors must be a non-negative number');
+        }
+        this._floors = floorCount;
+    }
+
+    evacuationWarningMessage() {
+        return `Evacuate slowly the ${this._floors} floors`;
+    }
+}
