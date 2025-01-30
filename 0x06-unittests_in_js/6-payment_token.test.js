c3f921a413af78a78a48a29e66b5a84c96c4d7da
ngari-kev <kevngariwangui@gmail.com>
2025-01-30 16:08:57 +0300

    async testing

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+const getPaymentTokenFromAPI = require("./6-payment_token");
+
+describe("getPaymentTokenFromAPI", () => {
+  it("should return a resolved promise with the correct data when success is true", (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        assert.deepStrictEqual(response, {
+          data: "Successful response from the API",
+        });
+        done();
+      })
+      .catch((error) => {
+        done(error);
+      });
+  });
+});
